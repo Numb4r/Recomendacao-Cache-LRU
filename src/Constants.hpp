f9b29427e79bf54ad0bd1aced7094a83cdd1e750
Numb4r <yuri.d.d.faria@gmail.com>
2020-09-25 19:01:49 -0300

    Create file for constants

---
+++ src/Constants.hpp
@@ -0,0 +1,36 @@
+#include <map>
+
+constexpr char *USERFILE = "users.dat";
+constexpr char *MOVIEFILE = "movies.dat";
+constexpr char *RATINGSFILE = "ratings.dat";
+const std::map<const int, const char *> Age{
+    {1, "Under 18"},
+    {18, "18-24"},
+    {25, "35-44"},
+    {35, "35-44"},
+    {45, "45-49"},
+    {50, "50-59"},
+    {56, "56+"}};
+
+const std::map<const int, const char *> Occupation{
+    {0, "other or not specified"},
+    {1, "academic/educator"},
+    {2, "artist"},
+    {3, "clerical/admin"},
+    {4, "college/grad student"},
+    {5, "customer service"},
+    {6, "doctor/health care"},
+    {7, "executive/managerial"},
+    {8, "farmer"},
+    {9, "homemaker"},
+    {10, "K-12 student"},
+    {11, "lawyer"},
+    {12, "programmer"},
+    {13, "retired"},
+    {14, "sales/marketing"},
+    {15, "scientist"},
+    {16, "self-employed"},
+    {17, "technician/engineer"},
+    {18, "tradesman/craftsman"},
+    {19, "unemployed"},
+    {20, "writer"}};
