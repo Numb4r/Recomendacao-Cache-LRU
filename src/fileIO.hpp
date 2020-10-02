d31e3ea929728b45c19def6ce95964046416af56
Numb4r <yuri.d.d.faria@gmail.com>
2020-10-02 14:45:00 -0300

    removido arquivo nao mais necessarios

--- src/fileIO.hpp
+++
@@ -1,8 +0,0 @@
-#include "Constants.hpp"
-#include <fstream>
-#include <vector>
-#include <iostream>
-#include <string>
-#include <cstring>
-
-std::vector<std::string> getInfoFile(const char *nameFile);
