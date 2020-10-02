e150d12a7ed97a309a7e9df702a52fc1c27919eb
Numb4r <yuri.d.d.faria@gmail.com>
2020-10-02 15:19:52 -0300

    leitura de arquivo

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
-#include "Constants.hpp"
-#include <fstream>
-#include <string>
 #include <cstring>
-
+#include "user.hpp"
+#include "movie.hpp"
+#include "rating.hpp"
+#include "list.hpp"
+#include <fstream>
 int main(int argc, char const *argv[])
 {
 
-    char *PATHFILES;
-    strcpy(PATHFILES, argc > 0 ? argv[1] : "ml-1m");
+        List<User> listUser;
+    List<Rating> listRating;
+    List<Movie> listMovie;
 
-    std::ifstream file{USERFILE};
-    std::cout << file.is_open() << std::endl;
-    std::string str;
-    while (file >> str)
+    std::ifstream file{"/home/yuri/Studies/C_C++/Recomendacao-Cache-LRU/ml-1m/ratings.dat"};
+    std::cout << file.good() << std::endl;
+    std::string s;
+    while (file >> s)
     {
-        std::cout << str << std::endl;
-    }
 
+        listRating.push_back(MakeRating(s.c_str(), ":"));
+    }
+    file.close();
+    file.open("/home/yuri/Studies/C_C++/Recomendacao-Cache-LRU/ml-1m/users.dat");
+    std::cout << file.good() << std::endl;
+
+    while (file >> s)
+    {
+        listUser.push_back(MakeUser(s.c_str(), ":"));
+    }
+    file.close();
+    file.open("/home/yuri/Studies/C_C++/Recomendacao-Cache-LRU/ml-1m/movies.dat");
+    std::cout << file.good() << std::endl;
+
+    while (file >> s)
+    {
+        listMovie.push_back(MakeMovie(s.c_str(), ":"));
+    }
     return 0;
 }
