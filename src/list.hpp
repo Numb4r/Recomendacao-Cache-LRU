99c614eead754fc2db492b8001c3a73d37bcb971
Numb4r <yuri.d.d.faria@gmail.com>
2020-10-02 15:12:48 -0300

    adequacao da lista

--- src/list.hpp
+++ src/list.hpp
@@ -1,9 +1,11 @@
-#include <vector>
-#include <string>
-#include <cstring>
-// #include <stringstream>
-#include <sstream>
-#include "users.hpp"
+/*
+>Essa lista e uma lista encabecada,ou seja,seu indice internamente se inicia em 1;
+>Essa lista e inversamente linkada 
+*/
+/*
+Eu deveria usar smart pointer para o problema de alocacao
+mas nao irei,pq e mais divertido ver meu eu do futuro sofrer com memory leak
+*/
 
 template <typename T>
 struct node
@@ -12,18 +14,6 @@ struct node
     struct node *next;
     struct node *previous;
 };
-/*
-Essa lista e uma lista encabecada
-Gastara um pouco mais de memoria ao custo de nao precisar verificar se e uma lista vazia a cada adicao
-Ou seja,seu indice internamente se inicia em 1;
-*/
-/*
-Essa lista e inversamente linkada tambem,pra ficar mais facil a remocao por tras
-*/
-/*
-Eu deveria usar smart pointer para o problema de alocacao
-mas nao irei,pq e mais divertido ver meu eu do futuro sofrer com memory leak
-*/
 template <typename T>
 class List
 {
@@ -39,11 +29,8 @@ public:
     int size();
     void push_back(T data);
     void pop_back();
-    // void push_front(T data);
-    // void pop_front();
     List();
-    List(const std::vector<std::string> infoArray, const char *tokens);
-    // ~List();
+    ~List();
 };
 template <typename T>
 List<T>::List()
@@ -54,17 +41,11 @@ List<T>::List()
     Node->previous = nullptr;
     HEAD = Node;
     TAIL = Node;
-    ++length;
 }
 template <typename T>
 void List<T>::push_back(const T data)
 {
     node<T> *Nd = new node<T>();
-    // if (!())
-    // {
-    //     throw "ERROR: CANNOT ALLOCATE MEMORY";
-    //     return;
-    // }
     Nd->data = data;
     TAIL->next = Nd;
     Nd->previous = TAIL;
@@ -95,33 +76,12 @@ node<T> *List<T>::tail()
 {
     return TAIL;
 }
-template <typename T>
-List<T>::List(const std::vector<std::string> infoVec, const char *tokens)
-{
-}
-template <>
-List<user>::List(const std::vector<std::string> infoVec, const char *tokens) : List<user>()
-{
 
-    /*
-    TODO: TROCAR ESSA MERDA POR REGEX OU AWK PUTA Q PARIU CODIGO FEIO DO KRL
-    */
-    for (auto &&i : infoVec)
+template <typename T>
+List<T>::~List()
+{
+    while (TAIL != nullptr)
     {
-        user us;
-        std::vector<std::string> tokens;
-        std::stringstream check(i);
-        std::string intermediante;
-        while (getline(check, intermediante, ':'))
-        {
-            tokens.push_back(intermediante);
-        }
-        us.UserID = tokens[0].c_str();
-        us.Gender = tokens[1].c_str();
-        us.Age = tokens[2].c_str();
-        us.Occupation = tokens[3].c_str();
-        us.ZipCode = tokens[4].c_str();
-        tokens.clear();
-        this->push_back(us);
+        this->pop_back();
     }
 }
