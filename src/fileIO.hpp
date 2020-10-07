c02ec39aa8b8809bc9da88f32afbd9ef0b8b71bc
Numb4r <yuridimitre@protonmail.com>
2020-10-07 20:49:19 -0300

    criada funcao para preencher uma lista qualquer e retornala

---
+++ src/fileIO.hpp
@@ -0,0 +1,19 @@
+#include "list.hpp"
+#include <fstream>
+#include <functional>
+template<typename T>
+List <T> fillList(const char* fileName,const char* token,std::function<T(const char*,const char*)> MakerT){
+    List<T> list;
+    std::ifstream file{fileName};
+    std::cout << file.good() << std::endl;
+    std::string s;
+    while (file >> s)
+    {
+
+        list.push_back(MakerT(s.c_str(), token));
+    }
+    file.close();
+    return std::move(list);
+}
+
+
