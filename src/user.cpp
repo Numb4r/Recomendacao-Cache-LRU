742d6c956f6d860aa98690cb6c367c474b2e8e09
Numb4r <yuridimitre@protonmail.com>
2020-10-07 20:46:52 -0300

    corrigido bug  de um atributo nao ser preenchido de forma devida

--- src/user.cpp
+++ src/user.cpp
@@ -1,6 +1,6 @@
 #include "user.hpp"
 #include <cstring>
-// #include <iostream>
+
 // #include "Constants.hpp"
 User MakeUser(const char *text, const char *tokens)
 {
@@ -21,19 +21,6 @@ User MakeUser(const char *text, const char *tokens)
 
     u.Occupation = pch;
     pch = strtok(NULL, tokens);
-    u.ZipCode;
+    u.ZipCode = pch;
     return u;
 }
-// User::User(std::vector<string> vec)
-//     : userID(vec[0]), gender(vec[1]), age(vec[2]), occupation(vec[3]), zipCode(vec[4])
-// {
-// }
-// void User::imprimir()
-// {
-//     std::cout
-//         << userID
-//         << ":" << gender
-//         << ":" << this->age.c_str()
-//         << ":" << this->occupation.c_str()
-//         << ":" << zipCode << std::endl;
-// }
