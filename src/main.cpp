181cc56842590c5750cea4fa6c03ccd88f5a4aaa
Numb4r <yuridimitre@protonmail.com>
2020-10-07 20:49:27 -0300

    reescrito main

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,23 @@
 #include <iostream>
-#include <cstring>
 #include "user.hpp"
 #include "movie.hpp"
 #include "rating.hpp"
 #include "list.hpp"
-#include <fstream>
-int main(int argc, char const *argv[])
+#include "fileIO.hpp"
+#include <functional>
+
+int main()
 {
+    
+    // std::function<User(const char*,const char*)> MU= MakeUser;
+    // std::function<Rating(const char*,const char*)>MR = MakeRating;
+    // std::function<Movie(const char*,const char*)>MM = MakeMovie;
+   
+    List<User>listUser(fillList<User>("ml-1m/users.dat",":", std::function<User(const char*,const char*)> (MakeUser)));
+    List<Rating> listRating(fillList<Rating>("ml-1m/ratings.dat",":", std::function<Rating(const char*,const char*)> (MakeRating)));
+    List<Movie> listMovie(fillList<Movie>("ml-1m/movies.dat",":", std::function<Movie(const char*,const char*)> (MakeMovie)));
+    std::cout<<listUser.size()<< "-"<<listRating.size()<<"-"<<listMovie.size()<<std::endl;
 
-        List<User> listUser;
-    List<Rating> listRating;
-    List<Movie> listMovie;
-
-    std::ifstream file{"ml-1m/ratings.dat"};
-    std::cout << file.good() << std::endl;
-    std::string s;
-    while (file >> s)
-    {
-
-        listRating.push_back(MakeRating(s.c_str(), ":"));
-    }
-    file.close();
-    file.open("ml-1m/users.dat");
-    std::cout << file.good() << std::endl;
-
-    while (file >> s)
-    {
-        listUser.push_back(MakeUser(s.c_str(), ":"));
-    }
-    file.close();
-    file.open("ml-1m/movies.dat");
-    std::cout << file.good() << std::endl;
-
-    while (file >> s)
-    {
-        listMovie.push_back(MakeMovie(s.c_str(), ":"));
-    }
+  
     return 0;
 }
