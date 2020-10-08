1f264114f54fd2b5ae2abb823d9267f054319a66
Numb4r <yuridimitre@protonmail.com>
2020-10-08 11:10:09 -0300

    Corrigido o bug que fazia com que a string nao seja copiada de forma correta

--- src/user.cpp
+++ src/user.cpp
@@ -1,7 +1,7 @@
 #include "user.hpp"
 #include <cstring>
+#include <iostream>
 
-// #include "Constants.hpp"
 User MakeUser(const char *text, const char *tokens)
 {
     User u;
@@ -9,18 +9,33 @@ User MakeUser(const char *text, const char *tokens)
     strcpy(str, text);
 
     char *pch = strtok(str, tokens);
-    u.UserID = pch;
+    u.UserID = new char[strlen(pch)+1];
+    strcpy(u.UserID,pch);
+    
 
     pch = strtok(NULL, tokens);
-    u.Gender = pch;
+    u.Gender = new char[strlen(pch)+1];
+    strcpy(u.Gender,pch);
+    
 
     pch = strtok(NULL, tokens);
+    u.Age = new char[strlen( pch)+1];
+    strcpy(u.Age,pch);
 
-    u.Age = pch;
     pch = strtok(NULL, tokens);
+    u.Occupation = new char[strlen(pch)+1];
+    strcpy(u.Occupation,pch);
 
-    u.Occupation = pch;
     pch = strtok(NULL, tokens);
-    u.ZipCode = pch;
+    u.ZipCode = new char[strlen(pch)+1];
+    strcpy(u.ZipCode,pch);
     return u;
+}
+void PrintUser(const User user){
+    std::cout
+    << "UserID: "<< user.UserID<<" "
+    << "\nGender: "<< user.Gender<<" "
+    << "\nAge: "<< user.Age<<" "
+    << "\nOccupation: "<< user.Occupation<<" "
+    << "\nZipCode: "<< user.ZipCode<<std::endl;
 }
--- src/user.hpp
+++ src/user.hpp
@@ -1,28 +1,12 @@
 #pragma once
 struct User
 {
-    const char *UserID;
-    const char *Gender;
-    const char *Age;
-    const char *Occupation;
-    const char *ZipCode;
+    char *UserID;
+    char *Gender;
+    char *Age;
+    char *Occupation;
+    char *ZipCode;
 };
 User MakeUser(const char *text, const char *tokens);
-
-// using string = std::string; //caso precise mudar o tipo para std::string
-
-// class User
-// {
-// private:
-//     string userID;
-//     string gender;
-//     string age;
-//     string occupation;
-//     string zipCode;
-
-// public:
-//     User() = default;
-//     void imprimir();
-
-//     User(std::vector<string> vec);
-// };
+void PrintUser(const User user);
+    
