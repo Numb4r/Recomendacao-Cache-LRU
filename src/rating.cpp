3a9a1feed32070000395c4d30e26b9103b36c743
Numb4r <yuridimitre@protonmail.com>
2020-10-08 11:22:25 -0300

    Corrigido o bug que fazia com que a string nao seja copiada de forma correta

--- src/rating.cpp
+++ src/rating.cpp
@@ -1,32 +1,7 @@
 #include "rating.hpp"
 #include <iostream>
 #include <cstring>
-// Rating::Rating(std::vector<string> vec) : userId(vec[0]), movieId(vec[1]), rating(vec[2]), timeStamp(vec[3])
-// {
-// }
-// void Rating::imprimir() const
-// {
-//     std::cout << movieId << "." << userId << "." << rating << "." << timeStamp << std::endl;
-// }
-// Rating::Rating(const char *text, const char *tokens)
-// {
-//     char str[strlen(text) + 1];
-//     strcpy(str, text);
 
-//     char *pch = strtok(str, ":");
-
-//     movieId = pch;
-//     pch = strtok(NULL, ":");
-//     userId = pch;
-
-//     pch = strtok(NULL, ":");
-
-//     rating = pch;
-//     pch = strtok(NULL, ":");
-
-//     timeStamp = pch;
-//     std::cout << movieId << "." << userId << "." << rating << "." << timeStamp << std::endl;
-// }
 Rating MakeRating(const char *text, const char *tokens)
 {
     Rating r;
@@ -34,16 +9,17 @@ Rating MakeRating(const char *text, const char *tokens)
     strcpy(str, text);
 
     char *pch = strtok(str, tokens);
-
-    r.movieId = pch;
+    r.movieId = new char[strlen(pch)+1];
+    strcpy(r.movieId,pch);
     pch = strtok(NULL, tokens);
-    r.userId = pch;
+    r.userId = new char[strlen(pch)+1];
+    strcpy(r.userId,pch);
 
     pch = strtok(NULL, tokens);
-
-    r.rating = pch;
+    r.rating = new char[strlen(pch)+1];
+    strcpy(r.rating,pch);
     pch = strtok(NULL, tokens);
-
-    r.timeStamp = pch;
+    r.timeStamp = new char[strlen(pch)+1];
+    strcpy(r.timeStamp,pch);
     return r;
 }
