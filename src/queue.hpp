b8304acf3e64903fd99619ff2d59d08946f7e36e
Numb4r <yuridimitre@protonmail.com>
2020-10-24 19:21:23 -0300

    rescrito container queue

--- src/queue.hpp
+++ src/queue.hpp
@@ -1,69 +1,30 @@
 #pragma once 
 #include <functional>
-template  <typename T>
-struct nodeQueue{
-    T data;
-    struct nodeQueue * next;
-};
-template <typename T>
-class Queue
-{
-private:
-    nodeQueue<T> *HEAD;
-    nodeQueue<T> *TAIL;
-    size_t size{0};
-public:
-    Queue(){
-        nodeQueue<T> *n = new nodeQueue<T>;
-        n->data = T();
-        n->next = nullptr;
-        HEAD = n;
-        TAIL = n;
-    }
-    /*
-    TODO : implementar copy/move constructor/assignment
-    */
-    ~Queue(){
-        nodeQueue<T> *n = HEAD;
-        do
-        {
-            HEAD = HEAD->next;
-            delete n;
-            n = HEAD;
-        } while (n!=nullptr);
-    }
-    void print(std::function<void(const T)> f){
-          nodeQueue<T> *n = HEAD->next;
-        while (n!=nullptr)
-        {
-            f(n->data);
-            n = n->next;
-        }
-    }
-    void push(const T data){
-        TAIL->next = new nodeQueue<T>;
-        TAIL->next->next=T();
-        TAIL->next->data = data;
-        TAIL = TAIL->next;
-        size++;
-    }
-    void pop(){
-        nodeQueue<T> *n = HEAD->next;
-        HEAD->next = HEAD->next->next;
-        size--;
-        delete n;
-    }
-    T front(){
-        return HEAD->next->data;
-    }
-    T back(){
-        return TAIL->data;
-    }
-    // T operator=(){
-    //     T data =  HEAD->next->data;
-    //     this->pop();
-    //     return data;
-    // }
-    
-};
+#include "list.hpp"
 
+namespace ctn
+{
+    template <typename T>
+    class Queue : public List<T>
+    {
+    public:
+        Queue() : List<T>() {}
+        void push(const T data) override{
+            
+            this->TAIL->next = new node<T>;
+            this->TAIL->next->next=T();
+            this->TAIL->next->data = data;
+            this->TAIL = this->TAIL->next;
+            this->length++;
+        }
+        void pop() override{
+            node<T> *n = this->HEAD->next;
+            this->HEAD->next = this->HEAD->next->next;
+            this->length--;
+            delete n;
+        }
+        
+    };
+
+
+} // namespace ctn
