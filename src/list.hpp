0ebe28945f57bafdf45a92bdab4b584166b65710
Numb4r <yuridimitre@protonmail.com>
2020-10-27 16:34:09 -0300

    reescrita dos operadores=

--- src/list.hpp
+++ src/list.hpp
@@ -109,24 +109,45 @@ namespace ctn
             length = obj.length;
         }
 
-        //  List<T> &operator=(const List &obj){//Copy assignment
-        //     std::cout<<"&obj\n";
-        //     if(this != &obj){
-        //         delete HEAD;
-        //         delete TAIL;
-        //         HEAD = new node<T>(obj.HEAD);
-        //         TAIL = new node<T>(obj.TAIL);
-        //         length = obj.length;
-        //     }
-        //     return *this;
+         List<T> &operator=(const List &obj){//Copy assignment
+            std::cout<<"&obj\n";
+            if(this != &obj){
+                while (TAIL != nullptr)
+                {
+                    this->pop();
+                }
+                
+                if (obj.HEAD == nullptr || obj.HEAD->next == nullptr)
+                {
+                    std::cout<<"Empty\n";
+                    List();
+                }else{
+                    HEAD = new node<T>(*obj.HEAD);
+                    node<T> *nd = obj.HEAD->next;
+                    node<T> *ndlist= HEAD;
+                    while (nd != obj.TAIL->next)
+                    {
+                        // std::cout<<nd->data<<"\n";
+                        ndlist->next = new node<T>(nd->data,ndlist,nullptr);
+                        ndlist = ndlist->next;
+                        nd = nd->next;
 
-        // }
+                    }
+                    TAIL = ndlist;
+                    length = obj.length;
+                    
+                }
+            }
+            return *this;
+        }
 
         List<T> &operator=(const List &&obj){//Move assignment
         std::cout<<"&&obj\n";
             if(this != &obj){
-                delete HEAD;
-                delete TAIL;
+                while (TAIL != nullptr)
+                {
+                    this->pop();
+                }
                 HEAD = obj.HEAD;
                 TAIL = obj.TAIL;
                 length = obj.length;
