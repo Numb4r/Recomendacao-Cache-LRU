ecec09d98f396c7a3f36b369921620cd25d7d3aa
Numb4r <yuridimitre@protonmail.com>
2020-10-30 20:18:40 -0300

    reescrito main

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
-#include "user.hpp"
-#include "movie.hpp"
-#include "rating.hpp"
-#include "list.hpp"
 #include "fileIO.hpp"
-#include <functional>
-
-int main()
+#include "list.hpp"
+#include "queue.hpp"
+#include "stack.hpp"
+#include "matrix_analysis.hpp"
+#define MATRIXFILENAME  "ml-1m/ratings.dat"
+#define USERFILENAME    "ml-1m/dumb_user.dat"
+#define K 20
+using Matrix = ctn::List<ctn::List<itemMatriz>> ;
+/*
+score =((i.numeroDeFilmesSimilares/(1+(1*i.distance)))/(1+(1*i.distance)))
+*/
+int main(int argc, char const *argv[])
 {
-       
-    List<User>listUser(fillList<User>("ml-1m/users.dat",":", MakeUser));
-    List<Rating> listRating(fillList<Rating>("ml-1m/ratings.dat",":", MakeRating));
-    List<Movie> listMovie(fillList<Movie>("ml-1m/movies.dat",":", MakeMovie));
-    std::cout<<listUser.size()<< "-"<<listRating.size()<<"-"<<listMovie.size()<<std::endl;
     
-    // listUser.printFullList(PrintUser);
-    // listRating.printFullList(PrintRating);
-    // listMovie.printFullList(PrintMovie);
+    const char* fileUserPath= argc >= 2 ? argv[1] : USERFILENAME;
     
-
-  
+    // // std::vector<std::vector<itemMatriz>> o {MatrizFatoracao(MATRIXFILENAME)}; // 0.1s/0.2s mais rapido
+    Matrix MatrixDeFatoracao{MatrizFatoracao(MATRIXFILENAME)} ;
+    ctn::List<itemMatriz> User{UserRatings(fileUserPath)};
+        
+    ctn::List<euclidian_score> Notas;
+    for (auto i = MatrixDeFatoracao.head(); i != MatrixDeFatoracao.tail()->next; i=i->next)
+    {
+        Notas.push(CalcularDistanciaEuclidiana(i->data,User));
+    }
+    ctn::Stack<euclidian_score> PilhaMelhoresUsuarios{EncontrarKMelhoresUsuarios(Notas,K)};
+    PilhaMelhoresUsuarios.show([](euclidian_score i){
+        std::cout<<i.linha.front().UserId<<" - "<<i.numeroDeFilmesSimilares<<" - " <<i.numeroDeFilmesDiferentes<<" - "<< score(i)<<std::endl;
+    });
+    
     return 0;
 }
