13725ce00c80e67a1f4ea72695b0ad3a2347d7ee
Numb4r <yuridimitre@protonmail.com>
2020-10-30 18:44:04 -0300

    removidos arquivos que nao sao mais necessarios

--- src/movie.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-struct Movie
-{
-    char *MovieId;
-    char *Title;
-    char *Genres;
-};
-Movie MakeMovie(const char *text, const char *tokens);
-void PrintMovie(const Movie movie);
