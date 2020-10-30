47712b2f2af697a36be65ab686a8609927f45761
Numb4r <yuridimitre@protonmail.com>
2020-10-30 17:58:06 -0300

    criada lib pra ordenacao

---
+++ src/sorting.hpp
@@ -0,0 +1,24 @@
+#include "list.hpp"
+namespace sort
+{
+    template<typename T>
+    void Selection(ctn::List<T> &list,std::function<bool(T var1,T var2)> funcaoDeComparacao){
+        for (auto i = list.head(); i != list.tail(); i=i->next)
+        {
+            auto  jMin = i;
+            for (auto j = i->next; j != nullptr; j=j->next)
+            {   
+                if(funcaoDeComparacao(j->data,jMin->data)){
+                
+                    jMin = j;
+                }
+            }
+            if (jMin != i)
+            {
+                auto d  = i->data;
+                i->data=jMin->data;
+                jMin->data = d;
+            }
+        }  
+    }
+} // namespace sort
