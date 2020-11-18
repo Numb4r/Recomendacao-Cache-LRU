d0ab47ac1a166023110b1e74eef12fd7f6645ff4
Numb4r <yuridimitre@protonmail.com>
2020-11-18 20:21:44 -0300

    criado selection sort para um array in heap

--- src/sorting.hpp
+++ src/sorting.hpp
@@ -1,6 +1,12 @@
 #include "list.hpp"
 namespace sort
 {
+    template <typename T>
+    void swap(T &a,T &b){
+        T aux{a};
+        a = b;
+        b = aux;
+    }
     template<typename T>
     void Selection(ctn::List<T> &list,std::function<bool(T var1,T var2)> funcaoDeComparacao){
         for (auto i = list.head(); i != nullptr; i=i->next)
@@ -21,4 +27,26 @@ namespace sort
             }
         }  
     }
+    
+    template<typename T>
+    void Selection(T *array,unsigned int size_array,std::function<bool(T var1,T var2)> cmpFunction){
+        for (size_t i = 0; i < size_array-1; i++)
+        {
+            int jMin = i;               
+            for (size_t j = i+1; j < size_array; j++)
+            {
+                if(cmpFunction(array[j],array[jMin])){
+                    jMin = j;
+                    
+                }
+            }
+            if(jMin != i){
+                swap<T>(array[i],array[jMin]);
+ 
+            }
+            
+        }
+    }
+   
+
 } // namespace sort
