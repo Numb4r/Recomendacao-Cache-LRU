bdfa60e63ce4edd5966f651bab894d81f739a1cc
Numb4r <yuridimitre@protonmail.com>
2020-11-19 21:14:20 -0300

    Quick/Shell

--- src/sorting.hpp
+++ src/sorting.hpp
@@ -1,13 +1,29 @@
 #include "list.hpp"
+#pragma once
+template <typename T>
+void swap(T &a,T &b){
+    T aux{a};
+    a = b;
+    b = aux;
+}
+template <typename T>
+int partition(T *array,int low,int high,std::function<bool(T var1,T var2)> cmpFunction){
+    T pivot{array[high]};
+    int i = (low -1);
+    for (int j = low; j < high-1; j++)
+    {
+        if(cmpFunction(array[j],pivot)){
+            i++;
+            swap<T>(array[i],array[j]);
+        }
+    }
+    swap<T>(array[i+1],array[high]);
+    return (i+1);
+}
 namespace sort
 {
-    template <typename T>
-    void swap(T &a,T &b){
-        T aux{a};
-        a = b;
-        b = aux;
-    }
-    template<typename T>
+    /*Uso especifico para o tipo ctn::List*/
+   template<typename T>
     void Selection(ctn::List<T> &list,std::function<bool(T var1,T var2)> funcaoDeComparacao){
         for (auto i = list.head(); i != nullptr; i=i->next)
         {
@@ -27,7 +43,6 @@ namespace sort
             }
         }  
     }
-    
     template<typename T>
     void Selection(T *array,unsigned int size_array,std::function<bool(T var1,T var2)> cmpFunction){
         for (size_t i = 0; i < size_array-1; i++)
@@ -42,11 +57,50 @@ namespace sort
             }
             if(jMin != i){
                 swap<T>(array[i],array[jMin]);
- 
+
             }
             
         }
     }
-   
+    template <typename T> 
+    void Insertion(T *array,int size_array,std::function<bool(T v1,T v2)> cmpFunction){
+        for (size_t i = 1; i < size_array; i++)
+        {
+            T key = array[i];
+            int j = i -1;
+            while (j>=0 && cmpFunction(array[j],key))
+            {
+                array[j+1] = array[j];
+                j=j-1;
+            }
+            array[j+1] = key;
 
+        }
+        
+    }
+
+    template<typename T>
+    void Quick(T *array,int low,int high,std::function<bool(T var1,T var2)> cmpFunction){
+        if(low < high){
+            int pi = partition<T>(array,low,high,cmpFunction);
+            Quick(array,low,pi-1,cmpFunction);
+            Quick(array,pi+1,high,cmpFunction);
+        }
+    }
+    template <typename T>
+    void Shell(T *array,int size_array,std::function<bool(T var1,T var2)> cmpFunction){
+        for (size_t gap = size_array/2; gap > 0; gap/=2)
+        {
+            for (size_t i = gap; i < size_array; i++)
+            {
+                int j{};
+                T temp = array[i];
+                for (j = i; j >= gap && cmpFunction(array[j-gap],temp); j-=gap)
+                    array[j] = array[j-gap];
+                    
+                array[j] = temp;
+                
+            }
+        }
+    }
 } // namespace sort
